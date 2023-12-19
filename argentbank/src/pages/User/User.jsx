875a45920bd0608d5714855d7db92988f6979cca
Eric Filser <filser.eric@protonmail.com>
2023-12-19 19:27:27 +0100

    Add Account component + update User.jsx accordingly + delete unused css

--- argentbank/src/pages/User/User.jsx
+++ argentbank/src/pages/User/User.jsx
@@ -1,4 +1,5 @@
 import '../../styles/main.css';
+import Account from '../../components/Account/Account';
 
 function User() {
   return (
@@ -8,36 +9,21 @@ function User() {
         <button class="edit-button">Edit Name</button>
       </div>
       <h2 class="sr-only">Accounts</h2>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-          <p class="account-amount">$10,928.42</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p class="account-amount">$184.30</p>
-          <p class="account-amount-description">Current Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Account 
+        title="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+        description="Available Balance"
+      />
+      <Account
+        title="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+        description="Available Balance"
+      />
+      <Account
+        title="Argent Bank Credit Card (x8349)"
+        amount="$184.30"
+        description="Current Balance"
+      />
     </main>
   );
 }
