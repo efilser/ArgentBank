92cde593973d6fdbffec9bbcfa082be89ee5b580
Eric Filser <filser.eric@protonmail.com>
2023-12-28 11:07:43 +0100

    Add styles to the Error page

--- argentbank/src/pages/Error/Error.jsx
+++ argentbank/src/pages/Error/Error.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import '../../styles/main.css';
+import './Error.css'
 
 function Error() {
   return (
-    <section>
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/">Retourner sur la page d’accueil</Link>
+    <section className="error">
+      <h1 className="error-title">404</h1>
+      <p className="error-text">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-link">Retourner sur la page d’accueil</Link>
     </section>
   );
 }
